2e6ada02736c21f0f89e0ec9050744aa0cb06003
web-developer-ridoy <103379670+web-developer-ridoy@users.noreply.github.com>
2023-04-14 15:15:57 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+const inputbox = document.getElementById('input-box');
+const list = document.getElementById('list');
+
+function addTask(){
+    if(inputbox.value === ''){
+        alert('You must write something!')
+    }else{
+        let li = document.createElement('li')
+        li.innerHTML = inputbox.value;
+        list.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputbox.value = '';
+    saveData();
+}
+
+
+list.onclick = (e) =>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}
+
+
+function saveData(){
+    localStorage.setItem('data', list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML = localStorage.getItem('data');
+}
+showTask();
